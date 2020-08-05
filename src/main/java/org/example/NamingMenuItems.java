a8d12b9849e872d3c108729ff87a58d246661d76
Bartosz <barzak200@gmail.com>
2020-08-05 20:22:50 +0200

    Adjusting spaces between name and shortcut
    Looks ok

--- src/main/java/org/example/NamingMenuItems.java
+++ src/main/java/org/example/NamingMenuItems.java
@@ -15,21 +15,26 @@ public class NamingMenuItems {
         StringBuilder after = new StringBuilder();
 
         after.append(before);
-        int howManySpaces = 40 - (before.length() + shortcut.length());
+        int howManySpaces = 50 - (before.length() + shortcut.length());
 
-        // Trochę jednak to słabe jest bo ogarnąłem że 40 znaków to nie zawsze ta sama szerokość
-        // np. ... są "krótsze" niż mmm XD
+        if(menuItem.equals(controller.saveFile)) {
+            howManySpaces += 1;
+        } else if(menuItem.equals(controller.saveFileAs)) {
+            howManySpaces -= 4;
+        }
+
         after.append(" ".repeat(howManySpaces));
         after.append(shortcut);
-
-        System.out.println(after.toString().length());
 
         return after.toString();
     }
 
     public void setNamesForFilesOption() {
         controller.newFile.setText(makeNameWithShortcut(controller.newFile, "Ctrl+N"));
+
         controller.openFile.setText(makeNameWithShortcut(controller.openFile, "Ctrl+O"));
+        //controller.openFile.setText(addSpacesToNameWithThreeDots(controller.openFile.getText()));
+
         controller.saveFile.setText(makeNameWithShortcut(controller.saveFile, "Ctrl+S"));
         controller.saveFileAs.setText(makeNameWithShortcut(controller.saveFileAs, "Ctrl+Shift+S"));
         controller.openFile.setText(makeNameWithShortcut(controller.openFile, ""));
