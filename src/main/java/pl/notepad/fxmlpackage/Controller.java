69b35be8ad2bc09366c37f3f3923feec79f4f171
Bartosz <barzak200@gmail.com>
2020-08-07 09:27:51 +0200

    Add onAction methods and delete not used fx:id

--- src/main/java/pl/notepad/fxmlpackage/Controller.java
+++ src/main/java/pl/notepad/fxmlpackage/Controller.java
@@ -17,6 +17,8 @@ public class Controller {
     public MenuItem saveFileAs;
     @FXML
     public MenuItem exit;
+    @FXML
+    TextArea textArea;
 
     NamingMenuItems nm = new NamingMenuItems(this);
 
@@ -27,19 +29,73 @@ public class Controller {
 
     @FXML
     public void newFileOnAction() {
-        System.out.println("New File...");
+        System.out.println("newFile");
     }
-
-    @FXML
-    TextArea textArea;
-
+    
     @FXML
     public void saveFileOnAction() {
         System.out.println(textArea.getText());
+        System.out.println("saveFile");
+    }
+
+    @FXML
+    public void undoOnAction() {
+        System.out.println("undo");
+    }
+
+    public void saveFileAsOnAction(ActionEvent actionEvent) {
+        System.out.println("saveFileAs");
+    }
+
+    public void exitOnAction(ActionEvent actionEvent) {
         System.out.println("Save");
     }
 
-    public void editOnAction(ActionEvent actionEvent) {
-        System.out.println("edit");
+    public void redoOnAction(ActionEvent actionEvent) {
+        System.out.println("redo");
+    }
+
+    public void cutOnAction(ActionEvent actionEvent) {
+        System.out.println("cut");
+    }
+
+    public void copyOnAction(ActionEvent actionEvent) {
+        System.out.println("copy");
+    }
+
+    public void pasteOnAction(ActionEvent actionEvent) {
+        System.out.println("paste");
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+        System.out.println("delete");
+    }
+
+    public void selectAllOnAction(ActionEvent actionEvent) {
+        System.out.println("selectAll");
+    }
+
+    public void wordWrapOnAction(ActionEvent actionEvent) {
+        System.out.println("wordWrap");
+    }
+
+    public void fontOnAction(ActionEvent actionEvent) {
+        System.out.println("font");
+    }
+
+    public void zoomInOnAction(ActionEvent actionEvent) {
+        System.out.println("zoomIn");
+    }
+
+    public void zoomOutOnAction(ActionEvent actionEvent) {
+        System.out.println("zoomOut");
+    }
+
+    public void statusBarOnAction(ActionEvent actionEvent) {
+        System.out.println("statusBar");
+    }
+
+    public void aboutOnAction(ActionEvent actionEvent) {
+        System.out.println("about");
     }
 }
