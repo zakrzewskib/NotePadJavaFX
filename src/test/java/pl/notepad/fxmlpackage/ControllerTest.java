a3b91fa72f0382e04873629984ac7b63cab43319
Bartosz <barzak200@gmail.com>
2020-08-15 12:11:18 +0200

    Testing Controller (fxml) does not work

---
+++ src/test/java/pl/notepad/fxmlpackage/ControllerTest.java
@@ -0,0 +1,28 @@
+package pl.notepad.fxmlpackage;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pl.notepad.boxes.ConfirmBox;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    Controller controller;
+
+    @BeforeEach
+    void setUp() {
+        //Controller controller = Mockito.mock(Controller.class);
+        Controller controller = new Controller();
+    }
+
+    @Test
+    void displayConfirmBox() {
+        controller.displayConfirmBox("new");
+        controller.textArea.setText("");
+        assertEquals(controller.textArea.getText(), "");
+    }
+
+}
